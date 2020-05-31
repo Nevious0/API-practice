612199d3899e8aad662c344d097c5aa58304dada
Nevie <nevie@gmail.com>
2020-05-31 20:26:18 +0200

    Added a new file

---
+++ main.js
@@ -0,0 +1,29 @@
+document.getElementById("btn").addEventListener("click", getText);
+document.getElementById("users").addEventListener("click", getUsers);
+
+function getText() {
+  fetch("sample.txt")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("output").innerHTML = data;
+    })
+    .catch((err) => console.log(err));
+}
+
+function getUsers() {
+  fetch("users.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let output = '<h2 class="mb-4">Users</h2>';
+      data.forEach(function (user) {
+        output += `
+      <ul class="list-group mb-3">
+      <li class="list-group-item">ID: ${user.id}</li>
+      <li class="list-group-item">Name: ${user.name}</li>
+      <li class="list-group-item">Email: ${user.email}</li>
+      </ul>
+      `;
+      });
+      document.getElementById("output").innerHTML = output;
+    });
+}
